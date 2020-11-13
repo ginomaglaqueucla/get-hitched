244f22eca415bc64e29c6fcdecf20d6ed938e2e2
cat - lin <plummorgan@gmail.com>
2020-11-12 19:49:13 -0800

    setting up server

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-//this will be the server
+const express = require('express');
+const sequelize = require('./config/connection.js');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+//this turns on the connection to the server and db
+sequelize.sync({ force: false }).then(() => { //at the bottom of the file, we use the sequelize.sync() method to establish the connection to the database
+    app.listen(PORT, () => console.log(`Now listening at ${PORT}!`));
+});

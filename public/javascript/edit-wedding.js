26fbce2654ba37949516b69d4858b79a547586b9
ginomaglaque <ginomaglaque@gmail.com>
2020-11-22 15:42:41 -0800

    linked new wedding input to couple's account

--- public/javascript/edit-wedding.js
+++ public/javascript/edit-wedding.js
@@ -12,16 +12,17 @@ async function editWeddingFormHandler(event) {
   console.log(wedding_date, wedding_hashtag, wedding_location, wedding_details);
   
   if (wedding_date && wedding_hashtag && wedding_location && wedding_details) {
+      console.log("teet rtet");
       const response = await fetch('/api/wedding', {
           method: 'POST',
           body: JSON.stringify({
               wedding_date,
-              wedding_hashtag,
               wedding_location,
+              wedding_hashtag,
               wedding_details
           }),
           headers: { 'Content-Type': 'application/json' }
-      })
+      });
       // check the response status
       if (response.ok) {
           console.log('You have successfully entered your wedding information.');

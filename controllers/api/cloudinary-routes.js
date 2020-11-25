819b8238a061f4036500507d56b1049b37bd60cb
ginomaglaque <ginomaglaque@gmail.com>
2020-11-24 16:12:24 -0800

    cloud fix check

--- controllers/api/cloudinary-routes.js
+++ controllers/api/cloudinary-routes.js
@@ -27,7 +27,7 @@ router.post('/upload', upload.single('weddingImage') , async (req, res, next) =>
     const cloudUpload = cloudinary.uploader.upload_stream(
         {
             // folder: newFolder 
-            folder: "tester"
+            folder: "test"
             // tags: "tester"
         },
         function(err, result) {
